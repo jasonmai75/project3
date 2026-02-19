4c237122c6c670904b5c2e5c076d78d9f69c3bf0
Fed19329 <letruonganhkha@gmail.com>
2026-02-18 20:30:31 -0800

    Parsing Error Test in Bussytem

--- testsrc/XMLBusSystemTest.cpp
+++ testsrc/XMLBusSystemTest.cpp
@@ -127,4 +127,102 @@ TEST(XMLBusSystemTest, PathTest){
     EXPECT_EQ(Path->GetNodeID(1), 315);
 }
 
+TEST(XMLBusSystemTest, ParsingErrorTest){
+    //reading nothing 
+    auto EmptySource = std::make_shared<CStringDataSource>("");
+    auto EmptyReader = std::make_shared< CXMLReader >(EmptySource);
+    auto BusPathSource = std::make_shared<CStringDataSource>(  "<paths>\n"
+                                                                "   <path source=\"321\" destination=\"311\">\n"
+                                                                "      <node id=\"321\"/>\n"
+                                                                "      <node id=\"315\"/>\n"
+                                                                "      <node id=\"311\"/>\n"
+                                                                "   </path>\n"
+                                                                "   <path source=\"345\" destination=\"370\">\n"
+                                                                "      <node id=\"345\"/>\n"
+                                                                "      <node id=\"356\"/>\n"
+                                                                "      <node id=\"367\"/>\n"
+                                                                "      <node id=\"370\"/>\n"
+                                                                "   </path>\n"
+                                                                "</paths>");
+    auto BusPathReader = std::make_shared< CXMLReader >(BusPathSource);
 
+    
+    CXMLBusSystem EmptyBusSystem(EmptyReader,BusPathReader);
+
+    ASSERT_EQ(EmptyBusSystem.StopCount(),0);
+    ASSERT_EQ(EmptyBusSystem.RouteCount(),0);
+    
+    //No stops at all
+    auto NoStopSource = std::make_shared<CStringDataSource>(  "<bussystem>\n"
+                                                                "<routes>\n"
+                                                                "   <route name=\"A\">"
+                                                                "       <routestop stop=\"1\" />"
+                                                                "       <routestop stop=\"2\" />"
+                                                                "       <routestop stop=\"3\" />"
+                                                                "   </route>\n"
+                                                                "   <route name=\"B\">"
+                                                                "       <routestop stop=\"3\" />"
+                                                                "       <routestop stop=\"2\" />"
+                                                                "       <routestop stop=\"1\" />"
+                                                                "   </route>\n"
+                                                                "</routes>\n"
+                                                                "</bussystem>");
+    auto NoStopReader = std::make_shared< CXMLReader >(NoStopSource);
+    CXMLBusSystem NoStopBusSystem(NoStopReader,BusPathReader);
+
+    ASSERT_EQ(NoStopBusSystem.StopCount(),0);
+    
+    //No end tag for stops
+    auto OnlyErrorStopsTagSource = std::make_shared<CStringDataSource>(  "<bussystem>\n"
+                                                                    "<stops>\n"                                                                 
+                                                                    "<routes>\n"
+                                                                    "   <route name=\"A\">"
+                                                                    "       <routestop stop=\"1\" />"
+                                                                    "       <routestop stop=\"2\" />"
+                                                                    "       <routestop stop=\"3\" />"
+                                                                    "   </route>\n"
+                                                                    "   <route name=\"B\">"
+                                                                    "       <routestop stop=\"3\" />"
+                                                                    "       <routestop stop=\"2\" />"
+                                                                    "       <routestop stop=\"1\" />");
+    auto OnlyErrorStopsTagReader = std::make_shared< CXMLReader >(OnlyErrorStopsTagSource);
+    CXMLBusSystem OnlyErrorStopsTagBusSystem(OnlyErrorStopsTagReader,BusPathReader);
+
+    ASSERT_EQ(OnlyErrorStopsTagBusSystem.StopCount(),0);
+    
+    //Have stop start tag but no end tag />
+    auto ErrorEndTagSource = std::make_shared<CStringDataSource>(  "<bussystem>\n"
+                                                                "<stops>\n"
+                                                                "   <stop id=\"1\" node=\"321\" description=\"First\">\n"                                                                 
+                                                                );
+    auto ErrorEndTagReader = std::make_shared< CXMLReader >(ErrorEndTagSource);
+    CXMLBusSystem ErrorEndTagBusSystem(ErrorEndTagReader,BusPathReader);
+
+    ASSERT_EQ(ErrorEndTagBusSystem.StopCount(),1);
+    ASSERT_EQ(ErrorEndTagBusSystem.RouteCount(), 0);
+
+    //No path
+    auto BusRouteSource = std::make_shared<CStringDataSource>(  "<bussystem>\n"
+                                                                "<stops>\n"
+                                                                "   <stop id=\"1\" node=\"321\" description=\"First\"/>\n"
+                                                                "   <stop id=\"2\" node=\"311\" description=\"second\"/>\n"
+                                                                "   <stop id=\"3\" node=\"300\" description=\"Third\"/>\n"
+                                                                "</stops>\n"
+                                                                "<routes>\n"
+                                                                "   <route name=\"A\">"
+                                                                "       <routestop stop=\"1\" />"
+                                                                "       <routestop stop=\"2\" />"
+                                                                "       <routestop stop=\"3\" />"
+                                                                "   </route>\n"
+                                                                "   <route name=\"B\">"
+                                                                "       <routestop stop=\"3\" />"
+                                                                "       <routestop stop=\"2\" />"
+                                                                "       <routestop stop=\"1\" />"
+                                                                "   </route>\n"
+                                                                "</routes>\n"
+                                                                "</bussystem>");
+    auto BusRouteReader = std::make_shared< CXMLReader >(BusRouteSource);
+    auto NoPathSource = std::make_shared<CStringDataSource>(" :) ");
+    auto NoPathReader = std::make_shared< CXMLReader >(NoPathSource);
+    CXMLBusSystem NoPathBusSystem(BusRouteReader,NoPathReader);
+}
